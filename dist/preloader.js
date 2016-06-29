de28f7cf31e621145fdea8a5a80a7752147a4d4d
yarkovaleksei <yarkov_aleksei@mail.ru>
2016-06-29 10:44:47 +0300

    Release v1.0.0

--- dist/preloader.js
+++ dist/preloader.js
@@ -1,6 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	setTimeout(function(){
-		var preloader = document.querySelector('.preloader');
-		preloader.className = 'hidden';
-	}, 5000);
+document.addEventListener("DOMContentLoaded", function(event) {
+	(function(){
+		setTimeout(function(){
+			var round1 = document.querySelector('.css3.round1');
+			round1.className += ' hidden';
+		}, 2000);
+		setTimeout(function(){
+			var left = document.querySelector('.css3.part.left');
+			left.className += ' out';
+			var right = document.querySelector('.css3.part.right');
+			right.className += ' out';
+		}, 3000);
+		setTimeout(function(){
+			var preloader = document.querySelector('.css3.container');
+			preloader.className += ' fadeOut';
+		}, 3500);
+	})();
 });
